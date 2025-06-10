09c1a253169ed862946d325b6b1e58e428ace000
Josh <joshjanoe@gmail.com>
2025-06-10 16:52:28 -0400

    task 4 commit 1

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -31,7 +31,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleRemove = (item) => {
-        dispatch(removeItem({ name: item.name, quantity:state.items = state.items.filter(item => item.name !== action.payload) }));
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -256,9 +256,13 @@ function ProductList({ onHomeClick }) {
 
     const [addedToCart, setAddedToCart] = useState({});
 
+    const calculateTotalQuantity = () => {
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+         };
+
     const handleAddToCart = (product) => {
         dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
-      
+        
         setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
           ...prevState, // Spread the previous state to retain existing entries
           [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
