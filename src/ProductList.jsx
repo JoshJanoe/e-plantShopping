59283083184d0b425c686e5bda50adf14f39b9b7
Josh <joshjanoe@gmail.com>
2025-06-10 17:46:55 -0400

    fixing add-to-cart functionality 2

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,9 @@
+//jjj
 import React, { useState, useEffect } from 'react';
+import { useDispatch,useSelector} from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
-import addItem from './CartSlice';
+import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -100,7 +102,7 @@ function ProductList({ onHomeClick }) {
                 },
                 {
                     name: "Marigold",
-                    image: "https://cdn.pixabay.com/photo/2022/02/22/05/45/marigold-7028063_1280.jpg",
+                    image:"https://cdn.pixabay.com/photo/2022/02/22/05/45/marigold-7028063_1280.jpg",
                     description: "Natural insect repellent, also adds color to the garden.",
                     cost: "$8"
                 },
@@ -261,17 +263,9 @@ function ProductList({ onHomeClick }) {
     
     // State to hold total quantity of items in cart
     const [totalQuantity, setTotalQuantity] = useState(calculateTotalQuantity());
-        useEffect(() => {
-            // Update total quantity whenever cartItems change
-            setTotalQuantity(calculateTotalQuantity());
-            // Update addedToCart state based on cart items
-            const updatedAddedToCart = {};
-            cartItems.forEach(item => {
-                updatedAddedToCart[item.name] = true;
-            });
-            setAddedToCart(updatedAddedToCart);
-    
-        }, [cartItems]);
+    useEffect(() => {
+        setTotalQuantity(calculateTotalQuantity());
+    }, [cartItems]);
 
     const handleAddToCart = (product) => {
         dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
@@ -287,7 +281,7 @@ function ProductList({ onHomeClick }) {
                 <div className="tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <a href="/" onClick={(e) => handleHomeClick(e)}>
+                        <a href="https://joshjanoe.github.io/e-plantShopping/" onClick={(e) => handleHomeClick(e)}>
                             <div>
                                 <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
@@ -298,8 +292,19 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="/cart" onClick={(e) => handleCartClick(e)}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg><div className='cart_quantity_count'>{totalQuantity}</div></h1></a></div>
-
+                    <div> 
+                        <a href="#" onClick={(e) => handleCartClick(e)}>
+                            <h1 className='cart'>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                    <rect width="156" height="156" fill="none"></rect>
+                                    <circle cx="80" cy="216" r="12"></circle>
+                                    <circle cx="184" cy="216" r="12"></circle>
+                                    <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                                </svg>
+                                <div className='cart_quantity_count'>{totalQuantity}</div>
+                            </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
@@ -327,8 +332,6 @@ function ProductList({ onHomeClick }) {
                                     disabled={addedToCart[plant.name]}
                                 >
                                     {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
-                                </button>
-                                    Add to Cart
                                 </button>
                                 </div>
                             ))}
