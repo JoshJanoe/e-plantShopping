eb13f0408c51d5cf6b8caafe556c0b90c2873271
Josh <joshjanoe@gmail.com>
2025-06-10 18:14:18 -0400

    fixing add-to-cart functionality 4

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,7 +8,7 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = (section) => {
+  const calculateTotalAmount = () => {
         let total = 0;
         cart.forEach((item) => {
             total += parseFloat(item.cost.substring(1)) * item.quantity;
@@ -20,25 +20,26 @@ const CartItem = ({ onContinueShopping }) => {
         onContinueShopping(e)
   };
 
-
-
   const handleIncrement = (item) => {
-        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
-
   const handleRemove = (item) => {
+    if (item.quantity>0){
     dispatch(removeItem(item.name));
+    }
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
-    let total = 0;
-    total += parseFloat(item.cost.substring(1)) * item.quantity;
-    return total;
+    return parseFloat(item.cost.substring(1)) * item.quantity;
   };
 
   return (
@@ -66,12 +67,10 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
-
-
